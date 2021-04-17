84af97faad31b8ce9d8d980fadeaab1c0f3dd708
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-04-17 19:01:35 +0100

    sección 4 embedding structs

--- seccion4/structs/main.go
+++ seccion4/structs/main.go
@@ -2,18 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip   int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// jose := person{"José", "Trujillo"}
 	// jose := person{firstName: "José", lastName: "Trujillo"}
 
-	var jose person
+	/* var jose person
 	jose.firstName = "Jose"
 	jose.lastName = "Trujillo"
+	jose.contact.email = "jose.hoc@gmail.com"
+	jose.contact.zip = 38007 */
+
+	jose := person{
+		firstName: "José",
+		lastName:  "Trujillo",
+		contact: contactInfo{
+			email: "jose.hoc@gmail.com",
+			zip:   38007,
+		},
+	}
 
 	fmt.Println(jose)
 	fmt.Printf("%+v\n", jose)
