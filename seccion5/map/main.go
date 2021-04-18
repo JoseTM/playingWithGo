50d78ef09e1c2eb5f9e0a7cf414096e8678c08cf
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-04-18 01:36:39 +0100

    seccion 5 maps,    - definición de maps vacíos    - asignación de un valor, acceso por key    - borrado de una clave-valor

--- seccion5/map/main.go
+++ seccion5/map/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
+	/* 	colors := map[string]string{
 		"red":   "#ff000",
 		"green": "#00ff00",
 		"blue":  "#0000ff",
-	}
+	} */
 
+	//var colors map[string]string
+
+	colors := make(map[string]string)
+
+	colors["white"] = "ffffff"
+	fmt.Println(colors)
+
+	delete(colors, "white")
 	fmt.Println(colors)
 }
