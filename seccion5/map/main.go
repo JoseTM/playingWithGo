c9891576e300b83bf93785cc48fe6dbd2960235c
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2021-04-18 01:43:18 +0100

    seccion 5 maps,  recorriendo y pintando

--- seccion5/map/main.go
+++ seccion5/map/main.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	/* 	colors := map[string]string{
+	colors := map[string]string{
 		"red":   "#ff000",
 		"green": "#00ff00",
 		"blue":  "#0000ff",
-	} */
+		"white": "#ffffff",
+	}
 
-	//var colors map[string]string
-
-	colors := make(map[string]string)
-
-	colors["white"] = "ffffff"
 	fmt.Println(colors)
+	printMap(colors)
+}
 
-	delete(colors, "white")
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Printf("The hexadecimal value to %v is %v \n", color, hex)
+	}
 }
